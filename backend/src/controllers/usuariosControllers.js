60163c9c5a2f06fe16c614663187c9b669cb702a
Julio <juliocsreyes94@gmail.com>
2026-02-19 09:10:31 +0100

    Controladores de uusarios temrinado

--- backend/src/controllers/usuariosControllers.js
+++ backend/src/controllers/usuariosControllers.js
@@ -120,4 +120,203 @@ const firstLogin = async (req, res) => {
     }
 }
 
-module.exports = {createUser, firstLogin}
+const userProfile = async (req, res) => {
+    const {id} = req.user
+
+    if(!id){
+        return res.status(403).json({error: "No tienes permisos"})
+    }
+
+    try{
+
+        const results = await pool.query(
+            "SELECT usuarios.nombre, usuarios.apellidos, usuarios.email, roles.rol FROM usuarios JOIN roles ON usuarios.rol_id = roles.id WHERE usuarios.id=$1",
+            [id]
+        )
+
+        if(results.rows.length === 0){
+            return res.status(404).json({error: "Usuario no encontrado"})
+        }
+
+        return res.status(200).json({
+            user: results.rows[0]
+        })
+
+    }catch(error){
+        console.error(error)
+        return res.status(500).json({error:"Error del servidor"})
+    }
+}
+
+const ownersAndAdminsView = async (req, res) => {
+    const {id, rol_id} = req.user
+
+    if(!id || Number(rol_id) === 3){
+        return res.status(403).json({error:"No tienes permisos"})
+    }
+
+    try{
+        const companysRows = await pool.query(
+            "SELECT empresa_id FROM usuarios_empresas WHERE usuario_id = $1",
+            [id]
+        )
+
+        if(companysRows.rows.length === 0){
+            return res.status(404).json({error: "No tienes o perteneces a ninguna empresa"})
+        }
+
+        const companysID = companysRows.rows.map(ci => ci.empresa_id)
+
+        const findUsers = await pool.query(
+            "SELECT DISTINCT u.nombre, u.apellidos, u.email,e.nombre AS empresa, r.rol AS rol FROM  usuarios_empresas ue JOIN usuarios u ON ue.usuario_id = u.id JOIN roles r ON u.rol_id = r.id JOIN empresas e ON ue.empresa_id = e.id WHERE ue.empresa_id = ANY($1) AND u.id <> $2",
+            [companysID, id]
+        )
+
+        return res.status(200).json(findUsers.rows)
+    
+    
+    }catch(error){
+        console.error(error)
+        return res.status(500).json({error:"Error del servidor"})
+    }
+}
+
+
+
+const updateUsers = async(req, res) => {
+    const {id, rol_id} = req.user
+    const {email, nombre, apellidos} = req.body
+    const {id_usuario} = req.params
+
+    if(!id || Number(rol_id) === 3){
+        return res.status(403).json({error:"No tienes permisos"})
+    }
+
+    if(!id_usuario){
+        return res.status(404).json({error:"No hay seleccionado ningun usuario"})
+    }
+
+    try{
+
+        const chekOwner = await pool.query(
+            "SELECT empresa_id FROM usuarios_empresas WHERE usuario_id = $1",
+            [id]
+        )
+
+        if(chekOwner.rows.length === 0){
+            return res.status(403).json({error: "No tienes permisos"})
+        }
+
+        const companys = chekOwner.rows.map(c => c.empresa_id)
+        const checkOwnerCompanys = await pool.query(
+            "SELECT 1 FROM usuarios_empresas WHERE empresa_id = ANY($1) AND usuario_id = $2",
+            [companys, id_usuario]
+        )
+
+        if(checkOwnerCompanys.rows.length === 0 ){
+            return res.status(403).json({error: "No tienes permisos para modificar este usuario"})
+        }
+
+    if(!email && !nombre && !apellidos){
+        const noChanges = await pool.query(
+            "SELECT nombre, apellidos, email FROM usuarios WHERE id=$1",
+            [id_usuario]
+        )
+
+        return res.status(200).json({
+            message: "No se han realizado cambios",
+            user: noChanges.rows[0]
+        })
+    }
+
+        const values = []
+        const setParts = []
+        let query = "UPDATE usuarios SET "
+
+        if(nombre){
+            values.push(nombre)
+            setParts.push(`nombre = $${values.length}`)
+        }
+
+        if(apellidos){
+            values.push(apellidos)
+            setParts.push(`apellidos = $${values.length}`)
+        }
+
+        if(email){
+            values.push(email)
+            setParts.push(`email = $${values.length}`)
+        }
+
+        values.push(id_usuario)
+        query += setParts.join(", ") + ` WHERE id = $${values.length} RETURNING nombre, apellidos, email`
+
+        const updateUser = await pool.query(query, values)
+
+        //UPDATE usuarios SET nombre = $1, apellidos = $2, email = $3 WHERE id = $4
+
+        return res.status(200).json({
+            message: "Usuario modificado con exito",
+            Actualizacion: updateUser.rows[0]
+        })
+    }catch(error){
+    console.error(error)
+    return res.status(500).json({error: "Error del servidor"})
+    }
+}
+
+const deleteUser = async (req, res) => {
+    const {id, rol_id} = req.user
+    const {id_usuario} = req.params
+
+    if(!id || Number(rol_id) === 3){
+        return res.status(403).json({error:"No tienes permisos"})
+    }
+
+    if(!id_usuario){
+        return res.status(404).json({error:"No hay seleccionado ningun usuario"})
+    }
+
+    try{
+
+        const chekOwner = await pool.query(
+            "SELECT empresa_id FROM usuarios_empresas WHERE usuario_id = $1",
+            [id]
+        )
+
+        if(chekOwner.rows.length === 0){
+            return res.status(403).json({error: "No tienes permisos"})
+        }
+
+        const companys = chekOwner.rows.map(c => c.empresa_id)
+        const checkOwnerCompanys = await pool.query(
+            "SELECT 1 FROM usuarios_empresas WHERE empresa_id = ANY($1) AND usuario_id = $2",
+            [companys, id_usuario]
+        )
+
+        if(checkOwnerCompanys.rows.length === 0 ){
+            return res.status(403).json({error: "No tienes permisos para modificar este usuario"})
+        }
+
+        if(Number(id) === Number(id_usuario)){
+            return res.status(403).json({error: "No puedes eliminar tu propio usuario"})
+        }
+
+        const deletedUser = await pool.query(
+            "DELETE FROM usuarios WHERE id = $1 RETURNING nombre, apellidos",
+            [id_usuario]
+        )
+
+        return res.status(200).json({
+            message: "Usuario eliminado con exito",
+            userDeleted: deletedUser.rows[0]
+        })
+
+    }catch(error){
+        console.error(error)
+        return res.status(500).json({error: "Error del servidor"})
+    }
+
+}
+
+module.exports = {createUser, firstLogin, userProfile, ownersAndAdminsView, updateUsers, deleteUser}
--- backend/src/routes/usuariosRoutes.js
+++ backend/src/routes/usuariosRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express")
-const {createUser, firstLogin} = require("../controllers/usuariosControllers")
+const {createUser, firstLogin, userProfile, ownersAndAdminsView, updateUsers, deleteUser} = require("../controllers/usuariosControllers")
 const isAuth = require("../middleware/isAuth")
 
 const router = express.Router()
 
 router.post("/createUser", isAuth, createUser)
 router.post("/firstLogin", isAuth, firstLogin)
+
+router.get("/me", isAuth,userProfile)
+router.get("/getAll", isAuth, ownersAndAdminsView)
+
+router.put("/updateUser/:id_usuario", isAuth, updateUsers)
+
+router.delete("/deleteUser/:id_usuario", isAuth, deleteUser)
 
 module.exports = router
