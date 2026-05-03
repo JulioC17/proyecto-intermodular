bf9cc6eed1c8e867b727227c761c6a9151a5b728
Julio <juliocsreyes94@gmail.com>
2026-05-03 22:00:28 +0200

    cambio del transporter de nodemailer

--- backend/src/controllers/usuariosControllers.js
+++ backend/src/controllers/usuariosControllers.js
@@ -12,6 +12,9 @@ const transporter = nodemailer.createTransport({
 host: process.env.SMTP_HOST,
 port: process.env.SMTP_PORT,
 secure: false,
+requireTLS: true,
+logger: true,
+debug:true,
 auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
